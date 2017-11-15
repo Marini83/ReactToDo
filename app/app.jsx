d5a674f46176b0a2e72921abfbce4a6b38634fa3
Marin Rusi <m_rusi@yahoo.com>
2017-11-15 16:40:55 -0500

    second commit

--- app/app.jsx
+++ app/app.jsx
@@ -2,18 +2,14 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+var TodoApp = require('TodoApp');
 // Load foundation 
 $(document).foundation();
 // require('style!css!foundation-sites/dist/foundation.min.css');
 //app css
 require('style!css!sass!applicationStyles');
 
-
-
-
-
 ReactDOM.render(
-		<p>Boiler plate 3 project</p>,
+		<TodoApp/>,
 		document.getElementById('app')
-		);
-
+	);
